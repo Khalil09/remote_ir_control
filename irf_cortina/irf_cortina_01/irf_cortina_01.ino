dd869055b1afca7e508807a2cfe0d13efff49f6e
Khalil09 <khalilcarsten@cjr.org.br>
2017-03-27 19:59:36 -0300

    Consertando bugs

--- irf_cortina/irf_cortina_01/irf_cortina_01.ino
+++ irf_cortina/irf_cortina_01/irf_cortina_01.ino
@@ -1,11 +1,12 @@
-const int TIME_PRESSED = 500; // TEMPO DE INTERVALO EM QUE O BOTAO FICA PRESSIONADO
+const int TIME_PRESSED = 500; // INTERVALO EM QUE O BOTAO FICA PRESSIONADO
 
 int IRF_PIN = 10; // PINO DE ESTADO DO RALAY DO IRF
 int RALAY_DOWN = 9; // PINO DO RALAY DO BOTAO DE DECIDA
 int RALAY_UP = 8; // PINO DO RALAY DO BOTAO DE SUBIDA
 bool C = false;
 bool changeA, changeB;
-bool direcao = 0;  // 0 = abre 1 = fecha
+int direcao_down = 0;  // 0 = abre 1 = fecha
+int direcao_up = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -28,18 +29,24 @@ void loop() {
     Serial.println(C);
   }
 
-  if(C && direcao){
+  if(C && direcao_down >= 0){
     digitalWrite(RALAY_DOWN, LOW);
     delay(TIME_PRESSED);
     digitalWrite(RALAY_DOWN, HIGH);
     Serial.print("DESCENDO");
-    direcao = 0;
-  } else if(C && !direcao){
+    direcao_down++;
+    if(direcao_down == 2) direcao_down = -1;
+    
+  } else if(C && direcao_up >= 0){
     digitalWrite(RALAY_UP, LOW);
     delay(TIME_PRESSED);
     digitalWrite(RALAY_UP, HIGH);
     Serial.print("SUBINDO");
-    direcao = 1;
+    direcao_up++;
+    if(direcao_up == 2){
+      direcao_down = 0;
+      direcao_up = 0;
+    }
   }
   
   C = false;
